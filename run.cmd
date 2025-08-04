5414b5cdbf9eb9f41cfcf283bf7d26058da7d735
yanis_truc <128294262+yanistruc@users.noreply.github.com>
2025-08-05 00:56:04 +0200

    Added HTML

--- run.cmd
+++ run.cmd
@@ -2,27 +2,17 @@
 setlocal enabledelayedexpansion
 
 where python >nul 2>nul
-if %ERRORLEVEL% neq 0 (
-    echo [ERROR] Python introuvable dans le PATH
-    exit /b 1
-)
+if %ERRORLEVEL% neq 0 exit /b 1
 
 where node >nul 2>nul
-if %ERRORLEVEL% neq 0 (
-    echo [ERROR] Node.js introuvable dans le PATH
-    exit /b 1
-)
+if %ERRORLEVEL% neq 0 exit /b 1
 
 python languages\hello_python.py
-if %ERRORLEVEL% neq 0 (
-    echo [ERROR] Erreur lors de l'exécution de hello_python.py
-    exit /b 1
-)
+if %ERRORLEVEL% neq 0 exit /b 1
 
 node languages\hello_javascript.js
-if %ERRORLEVEL% neq 0 (
-    echo [ERROR] Erreur lors de l'exécution de hello_javascript.js
-    exit /b 1
-)
+if %ERRORLEVEL% neq 0 exit /b 1
+
+start "" languages\hello_html.html
 
 pause
